25a9e45419d1ba7b279e0bbe151fa8c99a645b83
Tu Vu <vuanhtu0305@gmail.com>
2020-06-05 16:47:43 -0500

    Added solution to exercise 2.3 Delete middle node

---
+++ chapter2/delete_mid_node.py
@@ -0,0 +1,16 @@
+class Node:
+    def __init__(self, val: int, next):
+        self.val = val
+        self.next = next
+
+def delete_mid_node(target: Node):
+    while target:
+        target.val = target.next.val
+        if not target.next.next:
+            target.next = None
+        target = target.next
+'''
+Intuition: Simple copy val from next and delete last element
+Time complexity: O(N)
+Space complexity: O(1)
+'''
