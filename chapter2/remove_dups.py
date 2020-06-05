5ca1610ee905287d3e857e14f09162ada1b6bc43
Tu Vu <vuanhtu0305@gmail.com>
2020-06-05 16:46:51 -0500

    Added solution to exercise 2.1 Remove Dups

---
+++ chapter2/remove_dups.py
@@ -0,0 +1,25 @@
+class Node:
+    def __init__(self, val: int, next):
+        self.val = val
+        self.next = next
+
+def remove_dups(h: Node) -> Node:
+    container, cur, prev = set(), h, None
+    while cur:
+        # If val already in container, remove it
+        if cur.val in container:
+            if not prev:
+                h = cur.next
+                cur = h
+            else:
+                prev.next = cur.next
+                cur = cur.next
+        else:
+            container.add(cur.val)
+            prev = cur
+            cur = cur.next
+    return h
+'''
+Time complexity:  O(N)
+Space complexity: O(N)
+'''
